59051c2f32c4b334f4f92aec08e33250eb814698
Ramon Ferreira Matos <ramonmatosferreira@gmail.com>
2025-09-16 17:31:17 -0300

    Zoom ok and fixed column overlay

--- panel-fc-frontend/src/pages/PanelView/index.tsx
+++ panel-fc-frontend/src/pages/PanelView/index.tsx
@@ -5,13 +5,12 @@ import { loadPanelConfig } from "../../utils/storage";
 import { ZoomIn, ZoomOut, RulerDimensionLine, ChevronLeft, ChevronRight } from "lucide-react";
 
 
-
 const PanelViewPage: React.FC = () => {
-    const [config, setConfig] = useState<PanelConfiguration | null>(null);
-    const [zoom, setZoom] = useState(1);
-    const [page, setPage] = useState(0);
+    const [zoom, setZoom] = useState(0.9);
     const containerRef = useRef<HTMLDivElement | null>(null);
 
+    const [config, setConfig] = useState<PanelConfiguration | null>(null);
+    const [page, setPage] = useState(0);
     const pageWidthMM = 3000;
 
     const columns = [
@@ -48,9 +47,53 @@ const PanelViewPage: React.FC = () => {
                     "size": "63A" 
                 }
             ]
+        },
+        {
+            "columnId": "C-3",
+            "type": "Saídas",
+            "dimensions": {
+                "height": 2100,
+                "width": 500,
+                "depth": 600
+            },
+            "position": 3,
+            "modules": [
+                { 
+                    "moduleId": "M-2", 
+                    "name": "Disjuntor QF2", 
+                    "size": "63A" 
+                }
+            ]
         }
     ]
 
+    const totalWidth = columns.reduce((sum, c) => sum + c.dimensions.width, 0) + 0;
+    const maxHeight = Math.max(...columns.map(c => c.dimensions.height)) + 0;
+
+    const marginX = totalWidth * 0.15;
+    const marginY = totalWidth * 0.1;
+
+    const paginateColumns = (columns: PanelColumn[], pageWidth: number) =>{
+        const pages: PanelColumn[][] = [];
+        let current: PanelColumn[] = [];
+        let currentWidth = 0;
+
+        for (const col of columns) {
+            if (currentWidth + col.dimensions.width > pageWidth && current.length > 0) {
+                pages.push(current);
+                current = [];
+                currentWidth = 0;
+            }
+            current.push(col);
+            currentWidth += col.dimensions.width;
+        }
+
+        if (current.length > 0) {
+            pages.push(current);   
+        }
+
+        return pages;
+    }
 
     return(
         <AppLayout>
@@ -62,12 +105,16 @@ const PanelViewPage: React.FC = () => {
                         <button
                             className="px-1 py-1 rounded bg-white hover:bg-gray-100 
                                 gray shadow text-[var(--color-foreground)]"
+
+                            onClick={() => setZoom(z => Math.max(0.5, z * 0.9))}
                         >
                             <ZoomOut size={20} />
                         </button>
                         <button
                             className="px-1 py-1 rounded bg-white hover:bg-gray-100 
                                 gray shadow text-[var(--color-foreground)]"
+                            
+                            onClick={() => setZoom(z => Math.min(1, z * 1.1))}
                         >
                             <ZoomIn size={20} />
                         </button>
@@ -81,47 +128,60 @@ const PanelViewPage: React.FC = () => {
                 <svg
                     width="100%"
                     height="100%"
-                    viewBox={`0 0 3000 2000`}
+                    viewBox={`0 0 ${totalWidth+2*marginX} ${maxHeight+2*marginY}`}
                     className="bg-white rounded-xl shadow-inner"
+                    style={{
+                        transform: `scale(${zoom})`, 
+                        transformOrigin: "center"
+                    }}
                 >
-                    <g>
-                        <rect 
-                            x={0}
-                            y={0}
-                            width={5000}
-                            height={4000}
-                            fill="white"
-                            stroke="black"
-                            strokeWidth={1/1}
-                        />
+                    <rect 
+                        x={0}
+                        y={0}
+                        width={totalWidth+2*marginX}
+                        height={maxHeight+2*marginY}
+                        fill="white"
+                        stroke="black"
+                        strokeWidth={1}
+                    />
 
-                        {columns.map(col => (
-                            <g>
-                                <rect
-                                    x={0}
-                                    y={0}
-                                    width={col.dimensions.width}
-                                    height={col.dimensions.height}
-                                    fill="#f9fafb"
-                                    stroke="#034C8C"
-                                    strokeWidth={1 / 1}
-                                />
-                                <text
-                                    x={col.dimensions.width / 2}
-                                    y={14}
-                                    fontSize={12}
-                                    textAnchor="middle"
-                                    fill="#023059"
-                                    style={{ fontWeight: 600 }}
-                                >
-                                    {`Coluna ${col.position}`}
-                                </text>
-                            </g>
-                        ))}
+                    <g transform={`translate(${marginX}, ${marginY})`}>
+                        {(() => {
+                            let offsetX = 0;
+
+                            return columns.map(col => {
+                                const { width, height } = col.dimensions;
+                                const x = offsetX;
+                                const y = maxHeight - height;
+                                offsetX += width;
+
+                                return (
+                                    <g key={col.columnId} transform={`translate(${x},${y})`}>
+                                        <rect
+                                            x={0}
+                                            y={0}
+                                            width={width}
+                                            height={height}
+                                            fill="#f9fafb"
+                                            stroke="#034C8C"
+                                            strokeWidth={1}
+                                        />
+                                        <text
+                                            x={width/2}
+                                            y={50}
+                                            fontSize={28}
+                                            textAnchor="middle"
+                                            fill="#023059"
+                                            style={{ fontWeight: 400 }}
+                                        >
+                                            {`Coluna ${col.position} - ${col.type}`}
+                                        </text>
+                                    </g>
+                                );
+                            })
+                        })()}
                     </g>
                 </svg>
-
-
             </div>
         </AppLayout>
     );

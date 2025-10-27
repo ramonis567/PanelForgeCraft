acf9972dbf0342752e607980e48d032229249d25
Ramon Ferreira Matos <ramonmatosferreira@gmail.com>
2025-10-27 16:16:16 -0300

    Add authentication barrier to catalog route

--- panel-fc-backend/src/features/catalog/catalog.routes.ts
+++ panel-fc-backend/src/features/catalog/catalog.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { CatalogController } from "./catalog.controller";
+import { authenticate } from "../../shared/middlewares/auth";
 
 const router = Router();
 
+router.use(authenticate);
 router.get("/", CatalogController.getAll);
 router.get("/:id", CatalogController.getById);
 
